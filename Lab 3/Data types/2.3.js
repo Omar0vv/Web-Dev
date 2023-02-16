45b52f7bb4fa09d7f25034566f155e963da5eb24
Omar0vv <118433898+Omar0vv@users.noreply.github.com>
2023-02-16 23:35:11 +0600

    Add files via upload

---
+++ Lab 3/Data types/2.3.js
@@ -0,0 +1,11 @@
+function readNumber(){  
+    let a;
+    do{
+        a = prompt("number");
+        if (a == null || a == ""){
+            return null;
+        }
+    }         
+    while(!isFinite(a))
+    return +a;         
+}
