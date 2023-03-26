5ac3b5576122ac750ebc475b442695bb0575edb2
Omar0vv <118433898+Omar0vv@users.noreply.github.com>
2023-03-26 17:21:43 +0600

    Add files via upload

---
+++ Lab-7/informatics/functions/b.py
@@ -0,0 +1,5 @@
+def power(a, n):
+    print(a**n)
+a, b = map(float, input().split())
+b = int(b)
+power(a, b)
---
+++ Lab-7/informatics/functions/c.py
@@ -0,0 +1,4 @@
+def XOR(x, y):
+    print(x^y)
+a, b = map(int, input().split())
+XOR(a, b)
